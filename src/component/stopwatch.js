dd9b1b194f6eda9f7b316fcb8ed8c5a73460dc9d
Rajeshvbridge <rajesh.r@vbridgehub.com>
2024-03-24 20:43:40 +0530

    first commit

--- src/component/stopwatch.js
+++ src/component/stopwatch.js
@@ -5,22 +5,32 @@ import { useState, useEffect } from "react"
 function Stopwatch(){
     const [buttonclicked, setbuttonclicked] = useState(true)
     const [elapsedSeconds, setElapsedSeconds] = useState(0);
+    const [intervalId, setIntervalId] = useState(null);
 
     const handlebuttonclick = () =>{
-        setbuttonclicked(prevState => !prevState)
+        if (!intervalId) {
+            const newIntervalId = setInterval(() => {
+                setElapsedSeconds(prevElapsedSeconds => prevElapsedSeconds + 1);
+            }, 1000);
+            // console.log(setIntervalId)
+            setIntervalId(newIntervalId);
+            setbuttonclicked(false)
+        }
+        
     }
+
+    const handleStop = () => {
+        clearInterval(intervalId);
+        setIntervalId(null);
+        setbuttonclicked(true)
+    };
 
     const handlereset = () =>{
+
+        clearInterval(intervalId);
         setElapsedSeconds(0)
+        setIntervalId(null);
     }
-
-    useEffect(() => {
-        const intervalId = setInterval(() => {
-            setElapsedSeconds(prevElapsedSeconds => prevElapsedSeconds + 1);
-        }, 1000);
-
-        return () => clearInterval(intervalId);
-    }, []);
 
     const minutes = Math.floor(elapsedSeconds / 60);
     const seconds = elapsedSeconds % 60;
@@ -30,7 +40,7 @@ function Stopwatch(){
             <h2>Stopwatch</h2>
             <p>Time: {minutes < 10 ? `${minutes}` : minutes}:{seconds < 10 ? `0${seconds}` : seconds}</p>
             <div>
-                {buttonclicked ? (<button type="button" onClick={handlebuttonclick}>Start</button>) : (<button type="button" onClick={handlebuttonclick}>Stop</button>)}
+                {buttonclicked ? (<button type="button" onClick={handlebuttonclick}>Start</button>) : (<button type="button" onClick={handleStop}>Stop</button>)}
                 <button type="button" onClick={handlereset}>Reset</button>
             </div>
         </div>
